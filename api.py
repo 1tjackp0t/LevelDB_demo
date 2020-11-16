1e2ce3ffd7228980696896bf44695f4751c33e6e
obuuu <ltjackpot@outlook.com>
2020-11-16 23:23:55 +0800

    fix bug

--- api.py
+++ api.py
@@ -3,14 +3,17 @@ from flask import Flask, abort, request
 from db import DbClient
 
 app = Flask(__name__)
-
+db = DbClient('/db')
 
 @app.route('/api/put', methods=['PUT'])
 def api_put():
-    values = request.get_json()
-    if values is None:
+    data = request.get_json()
+    if data is None:
         abort(400)
-    print(values)
+    if data.get('key') is None or data.get('value') is None:
+        abort(400)
+    print(data)
+    db.put(data.get('key'),data.get('value'))
     return 'success'
 
 
@@ -19,20 +22,25 @@ def api_get(key):
     if key is None:
         abort(400)
     print(key)
-
-    return 'success'
+    value = db.get(key)
+    if value is None:
+        return 'value not found'
+    else:
+        return value.decode()
 
 
 @app.route('/api/queryAll', methods=['GET'])
 def api_query_all():
-    return 'hello world'
+    res = db.query_all()
+    print(res)
+    return res
 
 
 @app.route('/api/delete/<key>', methods=['DELETE'])
 def api_delete(key):
     if key is None:
         abort(404)
-    print(key)
+    db.delete(key)
     return 'success'
 
 
@@ -42,5 +50,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=2020, debug=False)
-    db = DbClient('/db')
+    app.run(host='0.0.0.0', port=2020, debug=True)
+
